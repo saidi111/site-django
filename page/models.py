f362264cd955e29dd5a28c6f8edf1e71d55774ba
unknown <marwensaidi@gmail.com>
2022-01-19 00:24:41 +0100

    ajout model

--- page/models.py
+++ page/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.admin import ModelAdmin
 # Create your models here.
+class contact(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    subject=models.CharField(max_length=200)
+    message=models.TextField()
+class contactAdmin(ModelAdmin):
+    list_display = ('name','email','subject','message',)
+    list_filter = ('name',)
+    search_fields = ['name',]
